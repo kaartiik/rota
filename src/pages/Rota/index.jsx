9adbf59cdd9dc6a39626c8464ae9832d6cd2bc0d
Kaartiik <kaartiikofficial@gmail.com>
2024-05-05 20:12:46 +0100

    check for duplicate shift

--- src/pages/Rota/index.jsx
+++ src/pages/Rota/index.jsx
@@ -28,15 +28,29 @@ const Rota = () => {
 
   const handleSubmit = () => {
     const entry = {
-      id: `${currentDay}-${selectedName}`,
-      name: selectedName,
+      id: `${currentDay}-${selectedName.content}`,
+      nameId: selectedName.id,
+      name: selectedName.content,
       startTime: startTime,
       endTime: endTime === '23:00' ? 'Closing' : endTime
     };
-    const newEntries = selectedDay[currentDay] ? [...selectedDay[currentDay], entry] : [entry];
-    setSelectedDay({ ...selectedDay, [currentDay]: newEntries });
-    setShowModal(false);
+
+    const isDuplicate = selectedDay[currentDay] && checkDuplicate(selectedDay[currentDay], entry)
+
+    if(!isDuplicate) {
+      const newEntries = selectedDay[currentDay] ? [...selectedDay[currentDay], entry] : [entry];
+      setSelectedDay({ ...selectedDay, [currentDay]: newEntries });
+      console.log({ ...selectedDay, [currentDay]: newEntries });
+      setShowModal(false);
+   } else{
+      alert(`${selectedName.content} already has a shift for this day.`)
+   }
   };
+
+  //check if user already has shift
+  const checkDuplicate = (dayArray, newEntry) => {
+   return dayArray.some(obj => obj.nameId === newEntry.nameId);
+  }
 
   return (
     <div className="week-container">
@@ -56,9 +70,12 @@ const Rota = () => {
           <div className="modal-content">
             <h2>Add Schedule for {currentDay}</h2>
             <label htmlFor="name-select">Name:</label>
-            <select id="name-select" onChange={(e) => setSelectedName(e.target.value)} value={selectedName}>
+            <select id="name-select" onChange={(e) => {const selectElement = e.target;
+               const selectedOption = selectElement.options[selectElement.selectedIndex];
+               setSelectedName({id: selectedOption.id, content: selectElement.value})}} value={selectedName}>
               <option value="">Select a name</option>
-              {names.map(name => <option key={name.id} value={name.content}>{name.content}</option>)}
+              {names.filter(name => name.content !== selectedName)
+               .map(name => <option key={name.id} id={name.id} value={name.content}>{name.content}</option>)}
             </select>
             <label htmlFor="start-time">Start Time:</label>
             <input type="time" id="start-time" min="11:00" max="20:00" step="900" value={startTime} onChange={(e) => setStartTime(e.target.value)} />
